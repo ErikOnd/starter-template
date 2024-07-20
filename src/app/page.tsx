fad8456c0f0fc0949e1cdc7fd587b707d875ab4a
Erik <erik.ondra@yahoo.com>
2024-07-20 16:02:12 +0200

    update for README.md

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
         <Container className="mt-28 flex w-full max-w-5xl justify-center">
             <Headline as="h1">Next.js Starter Template</Headline>
             <Text>
-                This is a Next.js starter template pre-configured with ESLint, Prettier, Tailwind, Shadcn, and a
-                selection of basic components.
+                This Next.js starter template comes pre-configured with ESLint, Prettier, Tailwind, Shadcn, TypeScript
+                and a variety of basic components.
             </Text>
             <Button>Click me</Button>
         </Container>
